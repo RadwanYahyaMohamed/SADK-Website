63fa45bacf4ddfd7d1fb78ac333806d938f6aa37
Radwan Yahia Mohamed Galal Mahmoud <radwan.1424023@stemassiut.moe.edu.eg>
2026-05-31 03:02:29 +0300

    revert my setendpoint

--- js/appwrite.js
+++ js/appwrite.js
@@ -1,7 +1,7 @@
 import { Client, Account, Databases, Storage } from "appwrite";
 
 const client = new Client()
-    .setEndpoint("https://sadk.tech/v1")
+    .setEndpoint("https://fra.cloud.appwrite.io/v1")
     .setProject("69a8b588001ea207b07c");
 
 const account = new Account(client);
